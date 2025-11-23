2ac7aed93b5e290f2110487808cbdd7069d08864
sasty1 <sokunla33@gmail.com>
2025-11-24 00:24:04 +0100

    Add /manifest route

---
+++ app/manifest/route.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+
+export function GET() {
+  return NextResponse.json({
+    version: "1",
+    name: "CastKeeper",
+    icon: "https://castkeeper-tsf3.vercel.app/icon.png",
+    homepage: "https://castkeeper-tsf3.vercel.app",
+    requested_permissions: ["signer"],
+    frames: [
+      {
+        path: "/",
+        name: "CastKeeper",
+      }
+    ]
+  });
+}
