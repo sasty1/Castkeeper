ce494b1b192f19e414f1f123a8f0098e3a88d1d1
sasty1 <sokunla33@gmail.com>
2025-11-24 18:33:33 +0100

    Fix manifest + farcaster.json

--- app/manifest/route.ts
+++ app/manifest/route.ts
@@ -1,17 +1,22 @@
-import { NextResponse } from "next/server";
-
-export function GET() {
-  return NextResponse.json({
-    version: "1",
-    name: "CastKeeper",
-    icon: "https://castkeeper-tsf3.vercel.app/icon.png",
-    homepage: "https://castkeeper-tsf3.vercel.app",
-    requested_permissions: ["signer"],
-    frames: [
-      {
-        path: "/",
-        name: "Main"
-      }
-    ]
+export async function GET() {
+  return Response.json({
+    accountAssociation: {
+      header: "eyJmaWQiOjU0MzMzNiwidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweENFQUNiY0ZjNTcwZjFGNTRFNzg4NjJjRDk2QzU4NDY1NzEwRkYxN2EifQ",
+      payload: "eyJkb21haW4iOiJjYXN0a2VlcGVyLXRzZjMudmVyY2VsLmFwcCJ9",
+      signature: "PSMmYrKElwaj1XE4EOUrHWHN0sJPgXxqDvrt/i6/zIJ2UbObmIcX98TxkZb9uiQNWcVPzpmmI6JYypWbHdNDIBw="
+    },
+    frame: {
+      version: "1",
+      name: "CastKeeper",
+      iconUrl: "https://castkeeper-tsf3.vercel.app/icon.png",
+      homeUrl: "https://castkeeper-tsf3.vercel.app/",
+      requested_permissions: ["signer"],
+      frames: [
+        {
+          path: "/",
+          name: "Main"
+        }
+      ]
+    }
   });
 }
